04e0572edb0c7fc5d29f24b6f53dc4be047a28ef
Augusto Meza <augusto.meza@wizeline.com>
2018-08-20 16:02:35 -0500

    Parse size arguments

--- app.py
+++ app.py
@@ -6,20 +6,17 @@ if __name__ == '__main__':
     p.add_argument("--train-size", default=150000, type=int,
                    action="store", dest="train_sz",
                    help="Train sample size [150000]")
-    p.add_argument("--cifar-size", default=50000, type=int,
-                   action="store", dest="train_sz",
-                   help="Cifar sample size 0 for all [0]")
-    p.add_argument("--verbose", default=False,
-                   action="store", dest="verbose",
-                   help="Verbose output [False]")
-    p.add_argument("--force", default=False,
-                   action="store", dest="forceRecal",
-                   help="Force vector recalculation [False]")
+    p.add_argument("--cifar-size", default=0, type=int,
+                   action="store", dest="cifar_sz",
+                   help="Cifar sample size, 0 for all [0]")
+    p.add_argument("--lld-size", default=50000, type=int,
+                   action="store", dest="lld_sz",
+                   help="LLD sample size, 0 for all [50000]")
 
     opts = p.parse_args()
 
-LogoOrCifar(
-    train_sz=opts.train_sz,
-    cifar_len=1,
-    lld_len=1
-)
+    LogoOrCifar(
+        train_sz=opts.train_sz,
+        cifar_len=opts.cifar_sz,
+        lld_len=opts.lld_sz
+    )
